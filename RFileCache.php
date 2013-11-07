375b668b27b8d887837665a4ad6d071ff096cf71
i80586 <i80586@mail.ru>
2013-11-07 15:21:03 +0400

    refactoring

--- RFileCache.php
+++ RFileCache.php
@@ -11,40 +11,40 @@ namespace RLibrary;
  */
 class RFileCache
 {
-    /**
-     * Error constants
-     */
-    const ERR_DIR_NOT_EXISTS=1;
-    
-    /**
-     * Cache folder
-     * @property string 
-     */
-    private $cacheDir;
-    
+	/**
+	 * Error constants
+	 */
+	const ERR_DIR_NOT_EXISTS=1;
+
+	/**
+	 * Cache folder
+	 * @property string 
+	 */
+	private $cacheDir;
+
 	/**
 	 * Default expire time for all cache files
 	 * As seconds
 	 * @property integer 
 	 */
 	private $expire=null;
-	
+
 	/**
 	 * Current cache identifier
 	 * @property string 
 	 */
 	private $cacheIdentifier;
-	
-    /**
-     * Class constructor
-     * @param string $cacheFolder
-     */
-    public function __construct($cacheDir, $expire=null)
-    {
+
+	/**
+	 * Class constructor
+	 * @param string $cacheFolder
+	 */
+	public function __construct($cacheDir, $expire=null)
+	{
 		$this->setCacheDir($cacheDir);
 		$this->expire=$expire;
-    }
-	
+	}
+
 	/**
 	 * Catch undefined property and
 	 * set current cache identifier
@@ -54,28 +54,28 @@ class RFileCache
 	{
 		$this->cacheIdentifier=$name;
 	}
-    
-    /**
-     * Returns cache dir
-     * @return string
-     */
-    public function getCacheDir()
-    {
+
+	/**
+	 * Returns cache dir
+	 * @return string
+	 */
+	public function getCacheDir()
+	{
 		return $this->cacheDir;
-    }
-    
-    /**
-     * Sets cache dir
-     * @param string $dir
-     * @throws Exception
-     */
-    public function setCacheDir($dir)
-    {
+	}
+
+	/**
+	 * Sets cache dir
+	 * @param string $dir
+	 * @throws Exception
+	 */
+	public function setCacheDir($dir)
+	{
 		$this->cacheDir=($dir[strlen($dir) - 1] != DIRECTORY_SEPARATOR) ? ($dir . DIRECTORY_SEPARATOR) : $dir;
 		if(!is_dir($this->cacheDir))
 			throw new \Exception('Cache dir is not exists', self::ERR_DIR_NOT_EXISTS);
-    }
-	
+	}
+
 	/**
 	 * Generates cache hash
 	 * Format: identifier-expire
