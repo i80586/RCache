9be53ed82598d43284162f4d7e22dec707edbf18
i80586 <i80586@mail.ru>
2013-11-07 15:06:20 +0400

    removed method removeExpiredFiles()

--- RFileCache.php
+++ RFileCache.php
@@ -17,11 +17,6 @@ class RFileCache
     const ERR_DIR_NOT_EXISTS=1;
     
     /**
-     * Default expire time (one hour)
-     */
-    const DEFAULT_EXPIRE_TIME=3600;
-    
-    /**
      * Cache folder
      * @property string 
      */
@@ -33,8 +28,8 @@ class RFileCache
      */
     public function __construct($cacheDir)
     {
-	$this->setCacheDir($cacheDir);
-	$this->removeExpiredFiles();
+		$this->setCacheDir($cacheDir);
+		$this->removeExpiredFiles();
     }
     
     /**
@@ -43,7 +38,7 @@ class RFileCache
      */
     public function getCacheDir()
     {
-	return $this->cacheDir;
+		return $this->cacheDir;
     }
     
     /**
@@ -53,28 +48,8 @@ class RFileCache
      */
     public function setCacheDir($dir)
     {
-	$this->cacheDir=($dir[strlen($dir) - 1] != DIRECTORY_SEPARATOR) ? ($dir . DIRECTORY_SEPARATOR) : $dir;
-	if(!is_dir($this->cacheDir))
-	    throw new \Exception('Cache dir is not exists', self::ERR_DIR_NOT_EXISTS);
-    }
-        
-    /**
-     * Remove expired cache files
-     */
-    private function removeExpiredFiles()
-    {
-	$currDate=time();
-	
-	$dirHandler=opendir($this->cacheDir);
-	while(($file=readdir($dirHandler)) !== false)
-	{
-	    if(is_file($this->cacheDir . $file))
-	    {
-		$fileLastModified=filemtime($this->cacheDir . $file);
-
-		if(($currDate - $fileLastModified) >= self::DEFAULT_EXPIRE_TIME)
-		    unlink($this->cacheDir . $file);
-	    }
-	}
+		$this->cacheDir=($dir[strlen($dir) - 1] != DIRECTORY_SEPARATOR) ? ($dir . DIRECTORY_SEPARATOR) : $dir;
+		if(!is_dir($this->cacheDir))
+			throw new \Exception('Cache dir is not exists', self::ERR_DIR_NOT_EXISTS);
     }
 }
