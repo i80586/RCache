adcac8274a85433c811d2f65dcd6c090e350b3b0
i80586 <i80586@mail.ru>
2013-12-17 20:31:53 +0400

    created cache handler class RCaching

---
+++ RCaching.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RLibrary;
+
+/**
+ * Cache handler
+ *
+ * @author Rasim Ashurov <rasim.ashurov@gmail.com>
+ * @date 17 December 2013
+ */
+class RCaching
+{
+	/**
+	 * Currently cache handler
+	 * @var cache handler
+	 */
+	private $_cacheHandler = null;
+	
+	
+	
+}
