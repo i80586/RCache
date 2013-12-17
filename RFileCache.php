707ba9b12ca109071dbfa9424d700e4d075833c2
i80586 <i80586@mail.ru>
2013-12-17 20:26:18 +0400

    changes in abstraction

--- RFileCache.php
+++ RFileCache.php
@@ -11,6 +11,13 @@ namespace RLibrary;
  */
 class RFileCache extends \RLibrary\RCache
 {
+	/**
+     * Error constants
+     */
+    const ERR_DIR_NOT_EXISTS = 0x1;
+    const ERR_WRANG_DURATION = 0x2;
+	const ERR_DELETE_FILE = 0x3;
+	
     /**
      * Cache folder
      * @property string 
@@ -67,8 +74,60 @@ class RFileCache extends \RLibrary\RCache
             throw new \Exception('Cache dir is not exists', self::ERR_DIR_NOT_EXISTS);
         }
     }
+	
+	/**
+     * Read data from file
+     * @param string $filename
+     * @return mixed
+     */
+    protected function readData($filename)
+    {
+        $output = false;
 
-    /**
+        if (is_file($filename)) {
+            $fileContent = file_get_contents($filename);
+            $expireTime = substr($fileContent, 0, 11);
+			
+            if ($expireTime > time()) {
+                $output = unserialize(substr($fileContent, 10));
+            } else {
+                unlink($filename);
+			}
+        }
+
+        return $output;
+    }
+	
+	/**
+     * Write data to file
+     * @param string $filename
+     * @param string $data
+     * @param integer $duration
+     */
+    protected function writeData($filename, $data, $duration)
+    {
+        file_put_contents($filename, (time() + $duration) . serialize($data), LOCK_EX);
+    }
+	
+	/**
+	 * Remove cache data
+	 * @param string $filename
+	 * @return boolean
+	 */
+	protected function removeData($filename)
+	{
+		try {
+			if (is_file($filename)) {
+				unlink($filename);
+			}
+		} catch (Exception $e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
      * Save data in cache
      * @param string $identifier
      * @param mixed $data
@@ -126,6 +185,34 @@ class RFileCache extends \RLibrary\RCache
 
 		return $this->removeData($this->cacheDir . $this->generateCacheHash($identifier));
     }
+	
+	/**
+	 * Begin reading data from buffer
+	 * @param string $identifier
+	 */
+	protected function beginProcess($cacheFile)
+	{
+		$cacheData = $this->readData($cacheFile);
+
+        if (false !== $cacheData) {
+			echo $cacheData;
+            return false;
+        } else {
+			return ob_start();
+        }
+	}
+	
+	/**
+	 * End reading from buffer and write to file
+	 * @param string $cacheFile
+	 * @param integer $cacheDuration
+	 */
+	protected function endProcess($cacheFile, $cacheDuration)
+	{
+		$this->writeData($cacheFile, ob_get_contents(), $cacheDuration);
+        ob_flush();
+        ob_end_clean();
+	}
 
     /**
      * Start reading from buffer
