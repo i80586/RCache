d8226ada0d440404c98163d2b08e1904cdb05b1b
i80586 <rasim.ashurov@gmail.com>
2014-07-10 13:03:50 +0500

    added methods to cache method

--- Cache.php
+++ Cache.php
@@ -24,27 +24,61 @@ class Cache
 	 */
 	public function __construct(ICache $cacheHandler)
 	{
-		$this->setHandler($cacheHandler);
-	}
-	
-	/**
-	 * Get current cache handler
-	 * 
-	 * @return ICache
-	 */
-	public function getHandler()
-	{
-		return $this->_cacheHandler;
-	}
-	
-	/**
-	 * Set cache handler
-	 * 
-	 * @param ICache $cacheHandler
-	 */
-	public function setHandler(ICache $cacheHandler)
-	{
 		$this->_cacheHandler = $cacheHandler;
+	}
+	
+	/**
+	 * Get cache by identifier
+	 * 
+	 * @param string $identifier
+	 * @return mixed
+	 */
+	public function get($identifier)
+	{
+		return $this->_cacheHandler->get($identifier);
+	}
+	
+	/**
+	 * Save data
+	 * 
+	 * @param string $identifier
+	 * @param mixed $data
+	 * @param integer $duration
+	 */
+	public function set($identifier, $data, $duration)
+	{
+		$this->_cacheHandler->set($identifier, $data, $duration);
+	}
+	
+	/**
+	 * Remove data from cache by identifier
+	 * 
+	 * @param string $identifier
+	 * @return boolean
+	 */
+	public function drop($identifier)
+	{
+		return $this->_cacheHandler->drop($identifier);
+	}
+	
+	/**
+	 * Start reading from buffer
+	 * 
+	 * @param string $identifier
+	 * @param integer $duration
+	 * @return boolean
+	 */
+	public function start($identifier, $duration = 0)
+	{
+		return $this->_cacheHandler->start($identifier, $duration);
+	}
+	
+	/**
+	 * End reading from buffer and save to cache
+	 */
+	public function end()
+	{
+		$this->_cacheHandler->end();
 	}
 	
 }
