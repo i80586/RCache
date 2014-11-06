a473effe20b70cfcb1a49a0c14aa839d75ac19cd
i80586 <i80586@i80586-desktop.(none)>
2014-11-07 00:17:19 +0400

    spaces

--- src/RCache/Cache.php
+++ src/RCache/Cache.php
@@ -10,13 +10,14 @@ namespace RCache;
  */
 class Cache
 {
-	/**
-	 * Cache handler
-	 * 
-	 * @var ICache
-	 */
-	protected $_cacheHandler = null;
-    
+
+    /**
+     * Cache handler
+     * 
+     * @var ICache
+     */
+    protected $_cacheHandler = null;
+
     /**
      * Available cache types
      * 
@@ -28,16 +29,16 @@ class Cache
     ];
 
     /**
-	 * Class construction
-	 * 
-	 * @param ICache|string $cacheType
+     * Class construction
+     * 
+     * @param ICache|string $cacheType
      * @param array $args
-	 */
-	public function __construct($cacheType, array $args = [])
-	{
+     */
+    public function __construct($cacheType, array $args = [])
+    {
         $this->_cacheHandler = ($cacheType instanceof ICache) ? $cacheType : self::getCacheClass($cacheType, $args);
-	}
-    
+    }
+
     /**
      * Get cache class by type
      * 
@@ -51,71 +52,71 @@ class Cache
         if ( ! array_key_exists($type, self::$_availableTypes)) {
             throw new \Exception('Cache type not found');
         }
-        
-        $className =  __NAMESPACE__ . '\\' . self::$_availableTypes[$type];
+
+        $className = __NAMESPACE__ . '\\' . self::$_availableTypes[$type];
         return (new \ReflectionClass($className))->newInstanceArgs($args);
     }
 
-        /**
-	 * Get cache by identifier
-	 * 
-	 * @param string $identifier
-	 * @return mixed
-	 */
-	public function get($identifier)
-	{
-		return $this->_cacheHandler->get($identifier);
-	}
-	
-	/**
-	 * Save data
-	 * 
-	 * @param string $identifier
-	 * @param mixed $data
-	 * @param integer $duration
-	 */
-	public function set($identifier, $data, $duration)
-	{
-		$this->_cacheHandler->set($identifier, $data, $duration);
-	}
-	
-	/**
-	 * Remove data from cache by identifier
-	 * 
-	 * @param string $identifier
-	 * @return boolean
-	 */
-	public function drop($identifier)
-	{
-		return $this->_cacheHandler->drop($identifier);
-	}
-	
-	/**
-	 * Start reading from buffer
-	 * 
-	 * @param string $identifier
-	 * @param boolean|integer $duration
-	 * @return boolean
-	 */
-	public function start($identifier, $duration = false)
-	{
-		if (false === ($cacheData = $this->_cacheHandler->beginProcess($identifier, $duration))) {
-			return ob_start();
-		} else {
-			echo $cacheData;
-			return false;
-		}
-	}
-	
-	/**
-	 * End reading from buffer and save to cache
-	 */
-	public function end()
-	{
-		$this->_cacheHandler->endProcess(ob_get_contents());
-		
-		ob_flush();
-		ob_end_clean();
-	}
-	
+    /**
+     * Get cache by identifier
+     * 
+     * @param string $identifier
+     * @return mixed
+     */
+    public function get($identifier)
+    {
+        return $this->_cacheHandler->get($identifier);
+    }
+
+    /**
+     * Save data
+     * 
+     * @param string $identifier
+     * @param mixed $data
+     * @param integer $duration
+     */
+    public function set($identifier, $data, $duration)
+    {
+        $this->_cacheHandler->set($identifier, $data, $duration);
+    }
+
+    /**
+     * Remove data from cache by identifier
+     * 
+     * @param string $identifier
+     * @return boolean
+     */
+    public function drop($identifier)
+    {
+        return $this->_cacheHandler->drop($identifier);
+    }
+
+    /**
+     * Start reading from buffer
+     * 
+     * @param string $identifier
+     * @param boolean|integer $duration
+     * @return boolean
+     */
+    public function start($identifier, $duration = false)
+    {
+        if (false === ($cacheData = $this->_cacheHandler->beginProcess($identifier, $duration))) {
+            return ob_start();
+        } else {
+            echo $cacheData;
+            return false;
+        }
+    }
+
+    /**
+     * End reading from buffer and save to cache
+     */
+    public function end()
+    {
+        $this->_cacheHandler->endProcess(ob_get_contents());
+
+        ob_flush();
+        ob_end_clean();
+    }
+
 }
