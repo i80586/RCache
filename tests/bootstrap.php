6bf188dc9d8a919085ba018b3fb39f838fb31bcf
i80586 <i80586@mail.ru>
2016-08-10 17:07:54 +0400

    fixed autoload bootratp.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($className) {
-	if (is_file($filePath = '../src/' . str_replace('\\', '/', $className) . '.php')) {
+	if (is_file($filePath = 'src/' . str_replace('\\', '/', $className) . '.php')) {
 		require $filePath;
 	}
 });
